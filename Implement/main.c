37fce40e4adae5c7c854c8fa70a87ed9e02b7ebe
Mohitrao Pathru <73344883+mohitraopathru@users.noreply.github.com>
2021-09-05 23:24:02 +0530

    Add files via upload

---
+++ Implement/main.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include"header.h"
+int main()
+{
+int i;
+int choice;
+
+do
+{
+printf("\n\n ###### Welcome to Election/Voting 2021 #####");
+printf("\n\n 1. Cast the Vote");
+printf("\n 2. Find Vote Count");
+printf("\n 3. Find leading Candidate");
+printf("\n 0. Exit");
+
+printf("\n\n Please enter your choice : ");
+scanf("%d", &choice);
+
+switch(choice)
+{
+case 1: castVote();break;
+case 2: votesCount();break;
+case 3: getLeadingCandidate();break;
+default: printf("\n Error: Invalid Choice");
+}
+}while(choice!=0);
+
+
+getchar();
+
+return 0;
+}
