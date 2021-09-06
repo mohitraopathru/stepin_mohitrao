82dd29f5a84a70d569b90c50930bc97fb6d89cbc
Mohitrao Pathru <73344883+mohitraopathru@users.noreply.github.com>
2021-09-06 20:44:32 +0530

    project

---
+++ 3_Implementation/SRC/header.h
@@ -0,0 +1,17 @@
+/**
+ * @file header.h
+ * @author Mohitrao pathru
+ * @brief voting system
+ * @version 0.1
+ * @date 2021-09-05
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include<stdio.h>
+#ifndef __HEADER_H
+#define __HEADER_H
+void castVote();
+void votesCount();
+void getLeadingCandidate();
+#endif
